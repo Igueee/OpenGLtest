ad886fa4959fb2b3d8cae5b1019aa3d9d0313ac4
Iguee <ignacioazevedo09@icloud.com>
2025-12-02 19:24:17 -0300

    wrote vertex shader source as char

--- OpenGLtest/main.cpp
+++ OpenGLtest/main.cpp
@@ -22,6 +22,13 @@
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
 
+const char *vertexShaderSource = "#version 320 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}\0";
+
 int main() {
     // Initialize the GLFW library. Required before any other GLFW calls.
     if (!glfwInit()) {
